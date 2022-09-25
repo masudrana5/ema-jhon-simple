ec1e92cdb353224171d375a6b7935a48e504e608
masudrana5 <abdullainfo30@gmail.com>
2022-09-25 22:38:03 +0600

    cart calculation function added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import './Cart.css';
 
 const Cart = (props) => {
+   const { cart } = props;
+   let total = 0;
+   let shipping = 0;
+   for (const product of cart) {
+      total += product.price;
+      shipping += product.shipping;
+   }
+   const tax = parseFloat((total * 0.1).toFixed(2));
+   const grandTotal = total + shipping + tax;
    return (
-      <div>
-         <h1>cart container</h1>
-         <p>Product items: {props.cart.length}</p>
+      <div className='cart-container'>
+         <h2 className='cart-title'>Order Summary</h2>
+         <p>Selected Items: {cart.length}</p>
+         <p>Total Price: { total}</p>
+         <p>Shipping Charge: { shipping}</p>
+         <p>Tax: { tax}</p>
+         <h4>Grand Total: { grandTotal}</h4>
+
+
       </div>
    );
 };
